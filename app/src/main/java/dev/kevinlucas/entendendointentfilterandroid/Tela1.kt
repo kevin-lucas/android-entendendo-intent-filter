bef185f2784d9a40ee5f4e65ac6ac4c00a6db614
= <kevinlucas.tec.info@gmail.com>
2021-02-17 15:03:31 -0300

    Projeto criado

---
+++ app/src/main/java/dev/kevinlucas/entendendointentfilterandroid/Tela1.kt
@@ -0,0 +1,11 @@
+package dev.kevinlucas.entendendointentfilterandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela1)
+    }
+}
